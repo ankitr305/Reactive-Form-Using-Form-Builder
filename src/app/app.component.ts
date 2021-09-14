07d0cae9e44cc31a17daecbf42b494460276b9dd
ankitr305 <ankitrajurkar03@gmail.com>
2021-09-14 11:06:53 +0530

    Final commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,114 @@
-import { Component } from '@angular/core';
+import { Component, Injectable, OnInit } from '@angular/core';
+import { FormGroup, FormBuilder, Validators, Form } from '@angular/forms';
 
 @Component({
   selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  template: `
+    <h1>Reactive Form</h1>
+    <br /><br />
+    <div>
+      <h2>User Data</h2>
+      <form [formGroup]="userForm" (ngSubmit)="onSubmit()">
+        <div class="form-group">
+          <label>Name</label>
+          <input type="text" class="form-control" formControlName="name" />
+        </div>
+        <div *ngIf="userForm.controls['name'].hasError('required')">
+          Please enter the name
+        </div>
+        <div class="form-group">
+          <label>Email</label>
+          <input type="text" class="form-control" formControlName="email" />
+        </div>
+        <div *ngIf="userForm.controls['email'].hasError('required')">
+          Please enter the Email
+        </div>
+        <div formGroupName="address">
+          <div class="form-group">
+            <label>Street</label>
+            <input type="text" class="form-control" formControlName="street" />
+          </div>
+          <div *ngIf="street.hasError('required')">
+          Please enter the Street
+        </div>
+          <div class="form-group">
+            <label>City</label>
+            <input type="text" class="form-control" formControlName="city" />
+          </div>
+          <div *ngIf="city.hasError('required')">
+          Please enter the City
+        </div>
+          <div class="form-group">
+            <label>Postal Code</label>
+            <input type="text" class="form-control" formControlName="postalCode"/>
+          </div>
+          <div *ngIf="postalCode.hasError('required')">
+          Please enter the Postal Code
+        </div>
+          <div *ngIf="postalCode.hasError('maxlength')">
+            Pin Code should be less than 6 characters
+          </div>
+        </div>
+        <button type="submit" [disabled]="!userForm.valid" class="btn btn-default">Submit</button>
+      </form>
+    </div>
+  `,
+  styles: [
+    `
+      input.ng-invalid {
+        border-left: 5px solid red;
+      }
+      input.ng-valid {
+        border-left: 5px solid green;
+      }
+    `
+  ]
 })
-export class AppComponent {
-  title = 'ReactiveFormBuilderDemo';
+export class AppComponent implements OnInit{
+  userForm: FormGroup;
+  constructor(private _formBulider : FormBuilder){
+  }
+
+  ngOnInit(){
+    this.userForm = this._formBulider.group({
+      name :["Ankit",Validators.required],
+      email : ["email@email.com",Validators.required],
+      address : this._formBulider.group({
+         street : ["street no.",Validators.required] ,
+         city : ["city",Validators.required],
+         postalCode : [null,Validators.maxLength(6)]
+      })
+    })
+  }
+
+
+  /*userForm = new FormGroup({
+    name: new FormControl('Ankit', [Validators.required]),
+    email: new FormControl("email@email.com",Validators.required),
+    address: new FormGroup({
+      street: new FormControl("street no.",Validators.required),
+      city: new FormControl("city",Validators.required),
+      postalCode: new FormControl(null,
+        Validators.maxLength(6)
+      )
+    })
+  });*/
+  get postalCode() {
+    return (this.userForm.controls['address'] as FormGroup).controls[
+      'postalCode'
+    ];
+  }
+  get street() {
+    return (this.userForm.controls['address'] as FormGroup).controls[
+      'street'
+    ];
+  }
+  get city() {
+    return (this.userForm.controls['address'] as FormGroup).controls[
+      'city'
+    ];
+  }
+  onSubmit() { 
+    
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,8 +1,9 @@
-import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { FormsModule,ReactiveFormsModule } from '@angular/forms';
 
 @NgModule({
   declarations: [
@@ -10,7 +11,7 @@ import { AppComponent } from './app.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,FormsModule,ReactiveFormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
